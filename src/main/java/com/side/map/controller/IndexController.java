88a43a67e2b97a1de71ac5affe5e28ef42c7b069
runpenguin <run-penguin@naver.com>
2023-07-10 21:17:02 +0900

    google map api key를 application.yml에서 관리하도록 수정

--- src/main/java/com/side/map/controller/IndexController.java
+++ src/main/java/com/side/map/controller/IndexController.java
@@ -1,14 +1,21 @@
 package com.side.map.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
     
 
+    @Value("${google-map-api.key}")
+    public String googleMapApiKey;
+
+
     @GetMapping("/")
-    public String indexPage() {
+    public String indexPage(ModelMap modelMap) {
+        modelMap.addAttribute("googleMapApiKey", googleMapApiKey);
         return "/index";
     }
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,4 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -24,7 +25,7 @@
     <script src="/js/index.js"></script>
 
     <!-- google map -->
-	<script async src="https://maps.googleapis.com/maps/api/js?key=*&callback=initMap"></script>
+	<script defer src="https://maps.googleapis.com/maps/api/js?key=${googleMapApiKey}&callback=initMap"></script>
 
     
 </body>
