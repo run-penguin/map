8e08efe88f5351b8a5eef8323cb39bc7dd3551be
runpenguin <run-penguin@naver.com>
2023-07-11 17:55:36 +0900

    google map - marker 표시하기

--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -8,11 +8,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/index.css"></link>
 </head>
 <body>
 
     <div class="wrap">
+        <div class="header">
+
+            <div class="row">
+                <div class="col-auto">
+                    <select id="shipList" class="form-select"></select>
+                </div>
+                <div class="col-auto">
+                    <button id="showMarker" class="btn btn-primary">Marker</button>
+                </div>
+                
+                
+            </div>
+            
+        </div>
         <div id="map">
 
         </div>
@@ -22,10 +37,12 @@
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
-    <script src="/js/index.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
     <!-- google map -->
 	<script defer src="https://maps.googleapis.com/maps/api/js?key=${googleMapApiKey}&callback=initMap"></script>
+
+    <script src="/js/index.js"></script>
 
     
 </body>
--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,21 +1,47 @@
-let map;
+
+
+
+const ships = [
+    { id: 1, name: '테스트 선박1', current: { lat: 36.5, lon: 130.39 } },
+    { id: 2, name: '테스트 선박2', current: { lat: 36.0, lon: 130.39 } },
+    { id: 3, name: '테스트 선박3', current: { lat: 35.5, lon: 130.39 } }
+];
 
 window.onload = function() {
     
-    
-    
+    const shipList = $('#shipList');
+
+    $.each(ships, (shipIdx, shipDto) => {
+        shipList.append($('<option>', { value: shipDto.id, text: shipDto.name }));
+    });
 }
 
+let marker;
 function initMap() {
 
-    map = new google.maps.Map(document.getElementById('map'), {
+    const map = new google.maps.Map(document.getElementById('map'), {
         zoom: 7,
-        center: { lat: 36.014, lng: 129.39734 }
-        // minZoom: 6,
-        // maxZoom: 18,
-        // mapTypeControl: false,  // 맵 타입 컨트롤
-        // streetViewControl: false,  // 거리 뷰 컨트롤
-        // fullscreenControl: false,  // 전체 보기 컨트롤
-        // zoomControl: false  // 확대/축소 컨트롤
+        center: { lat: 36.014, lng: 129.39734 },
+        minZoom: 6,
+        maxZoom: 18,
+        mapTypeControl: false,  // 맵 타입 컨트롤
+        streetViewControl: false,  // 거리 뷰 컨트롤
+        fullscreenControl: false,  // 전체 보기 컨트롤
+        zoomControl: false  // 확대/축소 컨트롤
+    });
+
+    $('#showMarker').on('click', function() {
+
+        if (marker) marker.setMap(null);
+        
+        const shipId = $('#shipList').val();
+        const shipDto = ships[shipId - 1];
+        const shipCurrent = shipDto.current;
+
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(shipCurrent.lat, shipCurrent.lon),
+            map,
+            title: shipDto.name
+        });
     });
 }
